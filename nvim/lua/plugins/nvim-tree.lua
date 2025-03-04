d3f5edea97c20acea8c7e4fcb1e95bb7adefbbed
Cristian Acuña <c.acuna.sepulveda@gmail.com>
2025-03-03 23:34:06 -0300

    Agregados `nvim-tree` y `mini.icons`

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,61 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    local nvimtree = require("nvim-tree")
+    local wk = require("which-key")
+
+    -- recommended settings from nvim-tree documentation
+    vim.g.loaded_netwr = 1
+    vim.g.loaded_netwrPlugin = 1
+
+    nvimtree.setup({
+      view = {
+        width = 35,
+        relativenumber = true,
+      },
+      -- change folder arrow keys
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "-",
+              arrow_open = "+",
+            },
+          },
+        },
+      },
+      -- disable window_picker for explorer
+      -- to work well with window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      git = {
+        ignore = false,
+      },
+    })
+
+    -- set keymaps
+    -- local keymap = vim.keymap
+    --
+    -- keymap.set("n", "<leader>et", "<cmd>NvimTreeToggle<CR>", { desc = "File [E]xplorer [T]oggle" })
+    -- keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "File [E]xplorer [F]ind" })
+    -- keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "File [E]xplorer [C]ollapse" })
+    -- keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "File [E]xplorer [R]efresh" })
+
+    wk.add({
+      { "<leader>e", group = "file [e]xplorer" },
+      { "<leader>et", "<cmd>NvimTreeToggle<CR>", desc = "File [E]xplorer [T]oggle" },
+      { "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", desc = "File [E]xplorer [F]ind" },
+      { "<leader>ec", "<cmd>NvimTreeCollapse<CR>", desc = "File [E]xplorer [C]ollapse" },
+      { "<leader>er", "<cmd>NvimTreeRefresh<CR>", desc = "File [E]xplorer [R]efresh" },
+    }, { mode = "n" })
+  end,
+}
